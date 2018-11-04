4043666df5d3d84f398363e97c9637a075c3d7e9
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 05:09:21 -0500

    Implement DecryptPassword Payload

--- src/main/java/me/hwiggy/whmjava/payload/d/DecryptPasswordPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/d/DecryptPasswordPayload.java
@@ -1,4 +1,20 @@
 package me.hwiggy.whmjava.payload.d;
 
-public class DecryptPasswordPayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/decryptpassword/
+ */
+public class DecryptPasswordPayload extends Payload {
+
+    /***
+     *
+     * @param password The string to decrypt
+     */
+    public DecryptPasswordPayload(String password) {
+        super("DecryptPassword");
+        append("password2", password);
+    }
 }

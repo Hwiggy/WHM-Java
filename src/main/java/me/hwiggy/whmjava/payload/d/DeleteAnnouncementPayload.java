86fdd1485585c3882cf4aac51bf34334c14b6f23
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 05:10:33 -0500

    Implement DeleteAnnouncement Payload

--- src/main/java/me/hwiggy/whmjava/payload/d/DeleteAnnouncementPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/d/DeleteAnnouncementPayload.java
@@ -1,4 +1,20 @@
 package me.hwiggy.whmjava.payload.d;
 
-public class DeleteAnnouncementPayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/deleteannouncement/
+ */
+public class DeleteAnnouncementPayload extends Payload {
+
+    /***
+     *
+     * @param announcementID The ID of the announcement to be deleted
+     */
+    public DeleteAnnouncementPayload(int announcementID) {
+        super("DeleteAnnouncement");
+        append("announcementid", announcementID);
+    }
 }

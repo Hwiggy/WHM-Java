994d4e1745c037e9b492a1ac9c148ceb3bd89f2d
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 00:10:14 -0400

    Add AddBannedIP Payload

---
+++ src/main/java/me/hwiggy/whmjava/payload/AddBannedIPPayload.java
@@ -0,0 +1,22 @@
+package me.hwiggy.whmjava.payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/addbannedip/
+ */
+public class AddBannedIPPayload extends Payload {
+
+    /***
+     *
+     * @param ip The IP Address to ban
+     * @param reason Admin only reason
+     * @param days If passed, expires date is auto calculated
+     */
+    AddBannedIPPayload(String ip, String reason, int days) {
+        super("AddBannedIp");
+        append("ip", ip);
+        append("reason", reason);
+        append("days", days);
+    }
+}

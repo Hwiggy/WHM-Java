e63c454bb36dd15e7613d723740afb8262e0435e
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 05:08:18 -0500

    Implement DeactivateModule Payload

--- src/main/java/me/hwiggy/whmjava/payload/d/DeactivateModulePayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/d/DeactivateModulePayload.java
@@ -1,4 +1,22 @@
 package me.hwiggy.whmjava.payload.d;
 
-public class DeactivateModulePayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/deactivatemodule/
+ */
+public class DeactivateModulePayload extends Payload {
+
+    /***
+     *
+     * @param moduleType The module type to be deactivated
+     * @param moduleName The module name to be deactivated
+     */
+    public DeactivateModulePayload(String moduleType, String moduleName) {
+        super("DeactivateModule");
+        append("moduletype", moduleType);
+        append("modulename", moduleName);
+    }
 }

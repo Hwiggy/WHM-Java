21acf39b148ee158b30aeaf6d2800a6a0e0d225a
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 03:41:26 -0500

    Implement AddProjectTask Payload

--- src/main/java/me/hwiggy/whmjava/payload/a/AddProjectTaskPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/a/AddProjectTaskPayload.java
@@ -1,4 +1,72 @@
 package me.hwiggy.whmjava.payload.a;
 
-public class AddProjectTaskPayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/addprojecttask/
+ */
+public class AddProjectTaskPayload extends Payload {
+
+    /***
+     *
+     * @param projectID The id of the project the task is for
+     * @param dueDate The duedate for the task. Format YYYY-mm-dd
+     */
+    public AddProjectTaskPayload(int projectID, String dueDate) {
+        super("AddProjectTask");
+        append("projectid", projectID);
+        append("duedate", dueDate);
+    }
+
+    /***
+     *
+     * @param adminID The admin id to associate the task with
+     * @return This Payload, for chaining
+     */
+    public AddProjectTaskPayload withAdminID(int adminID){
+        append("adminid",adminID);
+        return this;
+    }
+
+    /***
+     *
+     * @param task The task title
+     * @return This Payload, for chaining
+     */
+    public AddProjectTaskPayload withTask(String task){
+        append("task", task);
+        return this;
+    }
+
+    /***
+     *
+     * @param notes The notes for the task
+     * @return This Payload, for chaining
+     */
+    public AddProjectTaskPayload withNotes(String notes){
+        append("notes", notes);
+        return this;
+    }
+
+    /***
+     *
+     * @param completed Has the task been completed
+     * @return This Payload, for chaining
+     */
+    public AddProjectTaskPayload withCompleted(boolean completed){
+        append("completed", completed);
+        return this;
+    }
+
+    /***
+     *
+     * @param billed Has the task been billed
+     * @return This Payload, for chaining
+     */
+    public AddProjectTaskPayload withBilled(boolean billed){
+        append("billed", billed);
+        return this;
+    }
 }
--- src/main/java/me/hwiggy/whmjava/payload/a/AddTicketNotePayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/a/AddTicketNotePayload.java
@@ -1,4 +1,52 @@
 package me.hwiggy.whmjava.payload.a;
 
-public class AddTicketNotePayload {
+import me.hwiggy.whmjava.payload.Payload;
+import org.json.JSONObject;
+
+/***
+ *
+ */
+public class AddTicketNotePayload extends Payload {
+
+    /***
+     *
+     * @param message The content of the ticket note
+     * @param ticketNum The Client Ticket Number ID to apply the note to
+     */
+    public AddTicketNotePayload(String message, String ticketNum) {
+        super("AddTicketNote");
+        append("message", message);
+        append("ticketnum", ticketNum);
+    }
+
+    /***
+     *
+     * @param message The content of the ticket note
+     * @param ticketID The id of the ticket in the database.
+     */
+    public AddTicketNotePayload(String message, int ticketID){
+        super("AddTicketNote");
+        append("message", message);
+        append("ticketid", ticketID);
+    }
+
+    /***
+     *
+     * @param markdown Should markdown be used on the ticket note output
+     * @return This Payload, for chaining
+     */
+    public AddTicketNotePayload withMarkdown(boolean markdown){
+        append("markdown", markdown);
+        return this;
+    }
+
+    /***
+     *
+     * @param attachments Optional base64 json encoded array of file attachments. Can be the direct output of a multipart-form-data form submission ($_FILES superglobal in PHP) or an array of arrays consisting of both a filename and data keys
+     * @return This Payload, for chaining
+     */
+    public AddTicketNotePayload withAttachments(JSONObject attachments){
+        append("attachments", attachments);
+        return this;
+    }
 }

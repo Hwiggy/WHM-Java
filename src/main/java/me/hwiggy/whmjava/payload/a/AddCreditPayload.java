b58645c55294e6b4c2906320b8055df3114d7b4f
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 01:34:57 -0500

    Add AddCredit Payload

---
+++ src/main/java/me/hwiggy/whmjava/payload/a/AddCreditPayload.java
@@ -0,0 +1,24 @@
+package me.hwiggy.whmjava.payload.a;
+
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/addcredit/
+ */
+public class AddCreditPayload extends Payload {
+
+    /***
+     *
+     * @param clientID The client ID to add credit for
+     * @param description Admin only notes for credit justification
+     * @param amount The amount of credit to add
+     */
+    public AddCreditPayload(int clientID, String description, float amount) {
+        super("AddCredit");
+        append("clientid", clientID);
+        append("description", description);
+        append("float", amount);
+    }
+}

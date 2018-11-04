067fefb860d1f9e41ec8c14b781ac58bc64419a6
Hunter Wignall <hwignall2000@gmail.com>
2018-11-03 23:28:53 -0400

    Create ActivateModule Payload

---
+++ src/main/java/me/hwiggy/whmjava/payload/ActivateModulePayload.java
@@ -0,0 +1,31 @@
+package me.hwiggy.whmjava.payload;
+
+import org.json.JSONObject;
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/activatemodule/
+ */
+public class ActivateModulePayload extends Payload {
+
+    /***
+     *
+     * @param moduleType The module type to be activated
+     * @param moduleName The module name to be activated
+     */
+    ActivateModulePayload(String moduleType, String moduleName) {
+        super("ActivateModule");
+        append("moduleType", moduleType);
+        append("moduleName", moduleName);
+    }
+
+    /***
+     *
+     * @param parameters An array of configuration parameters to set for the given module. Use GetModuleConfigurationParameters to obtain a list of fields for a given module.
+     * @return This Payload, for chaining
+     */
+    public ActivateModulePayload withParameters(JSONObject parameters){
+        append("parameters", parameters);
+        return this;
+    }
+}

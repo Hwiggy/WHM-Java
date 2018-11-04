5000e273c30677fe3c9ba90cec0acdd044f21f44
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 05:11:23 -0500

    Implement DeleteClient Payload

--- src/main/java/me/hwiggy/whmjava/payload/d/DeleteClientPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/d/DeleteClientPayload.java
@@ -1,4 +1,20 @@
 package me.hwiggy.whmjava.payload.d;
 
-public class DeleteClientPayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/deleteclient/
+ */
+public class DeleteClientPayload extends Payload {
+
+    /***
+     *
+     * @param clientID The ID of the client to be deleted
+     */
+    public DeleteClientPayload(int clientID) {
+        super("DeleteClient");
+        append("clientid", clientID);
+    }
 }

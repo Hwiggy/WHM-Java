56d3b4c168a568fcc5087eed26c4fcec4d8813d0
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 04:18:03 -0500

    Implement CloseClient Payload

--- src/main/java/me/hwiggy/whmjava/payload/c/CloseClientPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/c/CloseClientPayload.java
@@ -1,4 +1,20 @@
 package me.hwiggy.whmjava.payload.c;
 
-public class CloseClientPayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/closeclient/
+ */
+public class CloseClientPayload extends Payload {
+
+    /***
+     *
+     * @param clientID The ID of the client to close
+     */
+    public CloseClientPayload(int clientID) {
+        super("CloseClient");
+        append("clientid", clientID);
+    }
 }

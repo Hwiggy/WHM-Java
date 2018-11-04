a6ffd327a0014cd7e883153da9aec664b564fe29
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 05:15:49 -0500

    Implement DeleteOAuthCredential Payload

--- src/main/java/me/hwiggy/whmjava/payload/d/DeleteOAuthCredentialPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/d/DeleteOAuthCredentialPayload.java
@@ -1,4 +1,20 @@
 package me.hwiggy.whmjava.payload.d;
 
-public class DeleteOAuthCredentialPayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/deleteoauthcredential/
+ */
+public class DeleteOAuthCredentialPayload extends Payload {
+
+    /***
+     *
+     * @param credentialID The ID of the credential to be deleted
+     */
+    public DeleteOAuthCredentialPayload(int credentialID) {
+        super("DeleteOAuthCredential");
+        append("credentialid", credentialID);
+    }
 }

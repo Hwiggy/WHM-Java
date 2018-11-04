56a6edbc632cb9771da9ca554c3cd2e0dae2bc74
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 05:16:51 -0500

    Implement DeleteOrder Payload

--- src/main/java/me/hwiggy/whmjava/payload/d/DeleteOrderPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/d/DeleteOrderPayload.java
@@ -1,4 +1,20 @@
 package me.hwiggy.whmjava.payload.d;
 
-public class DeleteOrderPayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/deleteorder/
+ */
+public class DeleteOrderPayload extends Payload {
+
+    /***
+     *
+     * @param orderID The ID of the order to be deleted
+     */
+    public DeleteOrderPayload(int orderID) {
+        super("DeleteOrder");
+        append("orderid", orderID);
+    }
 }

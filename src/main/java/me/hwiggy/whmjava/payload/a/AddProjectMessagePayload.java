a28f1e3f35836ed24c8ab89e0dbaff78430935f7
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 02:59:50 -0500

    Add AddProjectMessage Payload

---
+++ src/main/java/me/hwiggy/whmjava/payload/a/AddProjectMessagePayload.java
@@ -0,0 +1,32 @@
+package me.hwiggy.whmjava.payload.a;
+
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/addcprojectmessage/
+ */
+public class AddProjectMessagePayload extends Payload {
+
+    /***
+     *
+     * @param projectID The id of the project the message is for
+     * @param message The message to add to the project
+     */
+    public AddProjectMessagePayload(int projectID, String message) {
+        super("AddProjectMessage");
+        append("projectid", projectID);
+        append("message", message);
+    }
+
+    /***
+     *
+     * @param adminID Override the admin id for the message. Not passing will set to the adminid of the api request
+     * @return This Payload, for chaining
+     */
+    public AddProjectMessagePayload withAdminID(int adminID){
+        append("adminid", adminID);
+        return this;
+    }
+}

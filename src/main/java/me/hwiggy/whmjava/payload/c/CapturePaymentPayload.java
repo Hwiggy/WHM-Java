437b2c46ad36a909de79b4b253764de07ca00e01
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 04:14:00 -0500

    Implement CapturePayment Payload

--- src/main/java/me/hwiggy/whmjava/payload/c/CapturePaymentPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/c/CapturePaymentPayload.java
@@ -1,4 +1,30 @@
 package me.hwiggy.whmjava.payload.c;
 
-public class CapturePaymentPayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/capturepayment/
+ */
+public class CapturePaymentPayload extends Payload {
+
+    /***
+     *
+     * @param invoiceID The ID of the pending order
+     */
+    public CapturePaymentPayload(int invoiceID) {
+        super("CapturePayment");
+        append("invoiceid", invoiceID);
+    }
+
+    /***
+     *
+     * @param CVV The CVV Number for the card being attempted
+     * @return This Payload, for chaining
+     */
+    public CapturePaymentPayload withCVV(String CVV){
+        append("cvv", CVV);
+        return this;
+    }
 }

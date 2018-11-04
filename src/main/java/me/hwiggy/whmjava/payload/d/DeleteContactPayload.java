f79a6c5802a9de11c44e8af52e4716aadb217a6d
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 05:14:30 -0500

    Implement DeleteContact Payload

--- src/main/java/me/hwiggy/whmjava/payload/d/DeleteContactPayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/d/DeleteContactPayload.java
@@ -1,5 +1,20 @@
 package me.hwiggy.whmjava.payload.d;
 
-public class DeleteContactPayload {
+import me.hwiggy.whmjava.payload.Payload;
 
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/deletecontact/
+ */
+public class DeleteContactPayload extends Payload {
+
+    /***
+     *
+     * @param contactID The ID of the contact to be deleted
+     */
+    public DeleteContactPayload(int contactID) {
+        super("DeleteContact");
+        append("contactid", contactID);
+    }
 }

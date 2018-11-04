23bd4cfdab65f955dc01077b512da7152bbda520
Hunter Wignall <hwignall2000@gmail.com>
2018-11-04 04:07:37 -0500

    Implement AffiliateActivate Payload

--- src/main/java/me/hwiggy/whmjava/payload/a/AffiliateActivatePayload.java
+++ src/main/java/me/hwiggy/whmjava/payload/a/AffiliateActivatePayload.java
@@ -1,4 +1,20 @@
 package me.hwiggy.whmjava.payload.a;
 
-public class AffiliateActivatePayload {
+import me.hwiggy.whmjava.payload.Payload;
+
+/***
+ * This class and its methods directly relate to the API documentation.
+ * API documentation can be found at the following URL:
+ * https://developers.whmcs.com/api-reference/affiliateactivate/
+ */
+public class AffiliateActivatePayload extends Payload {
+
+    /***
+     *
+     * @param userID The client ID to activate affiliate status for
+     */
+    public AffiliateActivatePayload(int userID) {
+        super("AffiliateActivate");
+        append("userid", userID);
+    }
 }
